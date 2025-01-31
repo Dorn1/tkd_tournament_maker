c499c5b84a306acffa9d64687f4440a649492eab
Kuba <janochajakub02@gmail.com>
2025-01-31 19:11:27 +0100

    Initial commit

---
+++ src/main/java/pl/tkd/tournaments/tkd_tournament_maker/TkdTournamentMakerApplication.java
@@ -0,0 +1,13 @@
+package pl.tkd.tournaments.tkd_tournament_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkdTournamentMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkdTournamentMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tkd/tournaments/tkd_tournament_maker/TkdTournamentMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tkd.tournaments.tkd_tournament_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkdTournamentMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
