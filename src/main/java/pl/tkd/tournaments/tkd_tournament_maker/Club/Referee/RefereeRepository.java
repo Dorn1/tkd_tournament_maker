93d40dbe8764f196353cdd9fcfe4808218cc4d64
Kuba <janochajakub02@gmail.com>
2025-03-19 15:58:08 +0100

    styling

--- src/main/java/pl/tkd/tournaments/tkd_tournament_maker/Club/Referee/RefereeRepository.java
+++ src/main/java/pl/tkd/tournaments/tkd_tournament_maker/Club/Referee/RefereeRepository.java
@@ -1,7 +1,6 @@
 package pl.tkd.tournaments.tkd_tournament_maker.Club.Referee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.tkd.tournaments.tkd_tournament_maker.Club.Competitor.Competitor;
 
 public interface RefereeRepository extends JpaRepository<Referee, Long> {
 }
