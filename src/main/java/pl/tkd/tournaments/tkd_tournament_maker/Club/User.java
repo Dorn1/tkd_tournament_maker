8064cfb49836ce592ca7bd7708b20d0e819d3e43
Kuba <janochajakub02@gmail.com>
2025-09-08 20:55:28 +0200

    ladder removing

--- src/main/java/pl/tkd/tournaments/tkd_tournament_maker/Club/User.java
+++ src/main/java/pl/tkd/tournaments/tkd_tournament_maker/Club/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Data
 @Entity
+@Table(name = "users")
 public abstract class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,11 +28,9 @@ public abstract class User implements UserDetails {
     private String password;
 
     @CreationTimestamp
-    @Column(updatable = false, name = "created_at")
     private LocalDate createdAt;
 
     @UpdateTimestamp
-    @Column(name = "updated_at")
     private LocalDate updatedAt;
 
     @Override
