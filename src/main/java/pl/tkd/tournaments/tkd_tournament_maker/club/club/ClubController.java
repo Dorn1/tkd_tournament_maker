15aeebc78ba9b3096dbf95b347054b2466023bc3
Kuba <janochajakub02@gmail.com>
2025-09-16 15:43:03 +0200

    progress

--- src/main/java/pl/tkd/tournaments/tkd_tournament_maker/club/club/ClubController.java
+++ src/main/java/pl/tkd/tournaments/tkd_tournament_maker/club/club/ClubController.java
@@ -51,44 +51,4 @@ public class ClubController {
 
     }
 
-    @PostMapping(value = "/newClub")
-    public ResponseEntity<String> newClub(@RequestParam String name) {
-        clubService.addClub(name);
-        logger.info("New club {} created", name);
-        return ResponseEntity.ok("Club added");
-    }
-
-    @PostMapping(value = "/newCompetitor")
-    public ResponseEntity<String> newCompetitor(@RequestParam String firstName,
-                                                @RequestParam String lastName,
-                                                @RequestParam boolean male,
-                                                @RequestParam Long age,
-                                                @RequestParam Long clubId) {
-        try {
-            clubService.addCompetitorToClub(firstName,
-                    lastName,
-                    male,
-                    age,
-                    clubId);
-            logger.info("New competitor {} {} added", firstName, lastName);
-            return ResponseEntity.ok("Competitor added");
-        } catch (ObjectNotFoundException e) {
-            logger.warn("attempt to access a non-existent club");
-            return ResponseEntity.status(404).body(e.getMessage());
-        }
-    }
-
-    @PostMapping(value = "/newReferee")
-    public ResponseEntity<String> newReferee(@RequestParam String firstName,
-                                             @RequestParam String lastName,
-                                             @RequestParam Long clubId) {
-        try {
-            clubService.addRefereeToClub(firstName, lastName, clubId);
-            logger.info("New referee {} {} added", firstName, lastName);
-        } catch (ObjectNotFoundException e) {
-            logger.warn("attempt to access a non-existent club");
-            return ResponseEntity.status(404).body(e.getMessage());
-        }
-        return ResponseEntity.ok("Referee added");
-    }
 }
